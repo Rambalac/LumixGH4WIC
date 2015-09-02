623c934b9de775ace89867b5f3e82f0fed0cec4d
Rambalac <rambalac+github@gmail.com>
2015-09-02 17:44:26 +0900

    Added missing files back

---
+++ LumixGH4WIC/BitmapFrameDecode.cs
@@ -0,0 +1,177 @@
+﻿using com.azi.Decoder.Panasonic;
+using com.azi.Decoder.Panasonic.Rw2;
+using com.azi.Filters;
+using com.azi.Filters.Converters;
+using com.azi.Filters.Converters.Demosaic;
+using com.azi.Filters.VectorMapFilters;
+using com.azi.Image;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using WIC;
+
+namespace LumixGH4WIC
+{
+    [ComVisible(true)]
+    [Guid("1253D0FF-6836-4DE9-A141-FEF5B4C55DAA")]
+    class BitmapFrameDecode : IWICBitmapFrameDecode
+    {
+        static readonly Guid GUID_WICPixelFormat16bppBGR565 = new Guid("6FDDC324-4E03-4BFE-B185-3D77768DC90A");
+        static readonly Guid GUID_WICPixelFormat16bppBGRA5551 = new Guid("05EC7C2B-F1E6-4961-AD46-E1CC810A87D2");
+
+        static readonly Guid GUID_WICPixelFormat24bppBGR = new Guid("6FDDC324-4E03-4BFE-B185-3D77768DC90C");
+        static readonly Guid GUID_WICPixelFormat24bppRGB = new Guid("6FDDC324-4E03-4BFE-B185-3D77768DC90D");
+
+        static readonly Guid GUID_WICPixelFormat32bppBGR = new Guid("6FDDC324-4E03-4BFE-B185-3D77768DC90E");
+        static readonly Guid GUID_WICPixelFormat32bppBGRA = new Guid("6FDDC324-4E03-4BFE-B185-3D77768DC90F");
+        static readonly Guid GUID_WICPixelFormat32bppRGB = new Guid("D98C6B95-3EFE-47D6-BB25-EB1748AB0CF1");
+        static readonly Guid GUID_WICPixelFormat32bppRGBA = new Guid("F5C7AD2D-6A8D-43DD-A7A8-A29935261AE9");
+
+        PanasonicExif exif;
+        Stream stream;
+        RawMap map;
+        RGB8Map rgbmap;
+
+        public BitmapFrameDecode(Stream _stream, PanasonicExif _exif)
+        {
+            exif = _exif;
+            stream = _stream;
+            map = new PanasonicRW2Decoder().DecodeMap(stream, exif);
+        }
+
+        public void CopyPalette(IWICPalette pIPalette)
+        {
+            Log.Trace("CopyPalette called");
+            unchecked
+            {
+                throw new COMException("No Palette", (int)0x88982f45);
+            }
+        }
+
+        public void CopyPixels(ref WICRect prc, uint cbStride, uint cbBufferSize, IntPtr pbBuffer)
+        {
+            Log.Trace($"CopyPixels called: ({prc.X} {prc.Y} {prc.Width} {prc.Height}) Stride: {cbStride}, Size: {cbBufferSize}");
+
+            try
+            {
+                if (rgbmap == null) BuildRGB();
+                Log.Trace($"CopyPixels rgbmap {rgbmap.Width}, {rgbmap.Height}");
+                for (int y = prc.Y; y < prc.Y + prc.Height; y++)
+                    Marshal.Copy(rgbmap.Rgb, rgbmap.Stride * y + prc.X * 3, new IntPtr(pbBuffer.ToInt64() + cbStride * (y - prc.Y)), prc.Width * 3);
+                //Log.Trace("CopyPixels finished");
+            }
+            catch (Exception e)
+            {
+                Log.Error("CopyPixels failed: " + e);
+                throw;
+            }
+
+        }
+
+        private void BuildRGB()
+        {
+            Log.Trace($"BuildRGB called");
+            var debayer = new AverageBGGRDemosaic();
+
+            var white = new WhiteBalanceFilter();
+            //white.AutoAdjust(color16Image);
+            var gamma = new GammaFilter();
+
+
+            var light = new LightFilter();
+            //light.AutoAdjust(color16Image);
+
+            var colorMatrix = new ColorMatrixFilter
+            {
+                Matrix = new[,]
+                    {
+                        {1.87f, -0.81f, -0.06f},
+                        {-0.16f, 1.55f, -0.39f},
+                        {0.05f, -0.47f, 1.42f}
+                    }.ToMatrix4x4()
+            };
+
+            //var compressor = new VectorRGBCompressorFilter();
+            var compressor = new VectorRGBCompressorFilter();
+            var filters = new IFilter[]
+            {
+                    debayer,
+                    //white,
+                    gamma,
+                    light,
+                    colorMatrix,
+                    compressor
+            };
+
+            var processor = new ImageProcessor(map, filters);
+            rgbmap = (RGB8Map)processor.Invoke();
+            map = null;
+            Log.Trace("BuildRGB finished");
+        }
+
+        public void GetColorContexts(uint cCount, ref IWICColorContext ppIColorContexts, out uint pcActualCount)
+        {
+            Log.Trace("GetColorContexts called");
+            throw new NotImplementedException();
+        }
+
+        public void GetMetadataQueryReader(out IWICMetadataQueryReader ppIMetadataQueryReader)
+        {
+            Log.Trace("GetMetadataQueryReader called");
+            ppIMetadataQueryReader = new MetadataEnumerator(exif);
+            Log.Trace("GetMetadataQueryReader finished");
+        }
+
+        private static WICPixelFormatGUID GuidToWICGuid(Guid g)
+        {
+            var b = g.ToByteArray();
+            WICPixelFormatGUID o;
+            o.Data4 = new byte[8];
+            o.Data1 = BitConverter.ToInt32(b, 0);
+            o.Data2 = BitConverter.ToInt16(b, 4);
+            o.Data3 = BitConverter.ToInt16(b, 6);
+            Array.Copy(b, 8, o.Data4, 0, 8);
+            return o;
+        }
+
+        public void GetPixelFormat(out WICPixelFormatGUID pPixelFormat)
+        {
+            Log.Trace("GetPixelFormat called");
+            //pPixelFormat = GuidToWICGuid(GUID_WICPixelFormat32bppBGRA);
+            pPixelFormat = GuidToWICGuid(GUID_WICPixelFormat24bppRGB);
+            Log.Trace("GetPixelFormat finished");
+        }
+
+        public void GetResolution(out double pDpiX, out double pDpiY)
+        {
+            Log.Trace("GetResolution called");
+            pDpiX = 96;
+            pDpiY = 96;
+            Log.Trace("GetResolution finished");
+        }
+
+        public void GetSize(out uint puiWidth, out uint puiHeight)
+        {
+            Log.Trace("GetSize called");
+            puiWidth = (uint)map.Width;
+            puiHeight = (uint)map.Height;
+            Log.Trace($"GetSize finished: {puiWidth}x{puiHeight}");
+        }
+
+        public void GetThumbnail(out IWICBitmapSource ppIThumbnail)
+        {
+            Log.Trace("GetThumbnail called");
+            var guid = Guid.Empty;
+            var pPreviewDecoder = RW2BitmapDecoder.GetImagingFactory().CreateDecoderFromStream(
+                                new StreamComWrapper(new MemoryStream(exif.Thumbnail)), ref guid,
+                                WICDecodeOptions.WICDecodeMetadataCacheOnDemand);
+            IWICBitmapFrameDecode pPreviewFrame;
+            pPreviewDecoder.GetFrame(0, out pPreviewFrame);
+            ppIThumbnail = pPreviewFrame;
+            Log.Trace("GetThumbnail finished");
+        }
+
+    }
+}
---
+++ LumixGH4WIC/RW2BitmapDecoder.cs
@@ -0,0 +1,298 @@
+﻿using com.azi.Decoder.Panasonic;
+using com.azi.Decoder.Panasonic.Rw2;
+using com.azi.tiff;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using WIC;
+using IStream = System.Runtime.InteropServices.ComTypes.IStream;
+
+namespace LumixGH4WIC
+{
+    [ComVisible(true)]
+    class MetadataEnumerator : IEnumUnknown, IWICMetadataQueryReader, IWICMetadataReader
+    {
+        private PanasonicExif exif;
+        private List<IfdBlock>.Enumerator enumerator;
+
+        public MetadataEnumerator(PanasonicExif exif)
+        {
+            this.exif = exif;
+            enumerator = exif.RawIfd.GetEnumerator();
+        }
+
+        public void Clone(out IEnumUnknown ppenum)
+        {
+            ppenum = new MetadataEnumerator(exif);
+        }
+
+        public void RemoteNext(uint celt, out object rgelt, out uint pceltFetched)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            enumerator = exif.RawIfd.GetEnumerator();
+        }
+
+        public void Skip(uint celt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetContainerFormat(out Guid pguidContainerFormat)
+        {
+            pguidContainerFormat = typeof(RW2BitmapDecoder).GUID;
+        }
+
+        public void GetLocation(uint cchMaxLength, ref ushort wzNamespace, out uint pcchActualLength)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetMetadataByName(string wzName, ref tag_inner_PROPVARIANT pvarValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetEnumerator(out IEnumString ppIEnumString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetMetadataFormat(out Guid pguidMetadataFormat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetMetadataHandlerInfo(out IWICMetadataHandlerInfo ppIHandler)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetCount(out uint pcCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetValueByIndex(uint nIndex, ref tag_inner_PROPVARIANT pvarSchema, ref tag_inner_PROPVARIANT pvarId, ref tag_inner_PROPVARIANT pvarValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetValue(ref tag_inner_PROPVARIANT pvarSchema, ref tag_inner_PROPVARIANT pvarId, ref tag_inner_PROPVARIANT pvarValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetEnumerator(out IWICEnumMetadataItem ppIEnumMetadata)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [Guid("CACAF262-9370-4615-A13B-9F5539DA4C0A")]
+    [ComImport]
+    class WICImagingFactory1 { }
+
+    [Guid("7B816B45-1996-4476-B132-DE9E247C8AF0")]
+    [ComImport]
+    class WICImagingFactory2 { }
+
+    [Guid("EC5EC8A9-C395-4314-9C77-54D7A935FF70")]
+    [ComImport]
+    class WICImagingFactory3 { }
+
+    [ComVisible(true)]
+    [Guid("DD48659C-F21F-4C15-AE70-6879ED43B84C")]
+    public class RW2BitmapDecoder : IWICBitmapDecoder
+    {
+        internal static IWICImagingFactory ImagingFactory;
+        internal static IWICImagingFactory GetImagingFactory()
+        {
+            if (ImagingFactory == null)
+                try
+                {
+                    ImagingFactory = (IWICImagingFactory)new WICImagingFactory1();
+                }
+                catch
+                {
+                    try
+                    {
+                        ImagingFactory = (IWICImagingFactory)new WICImagingFactory2();
+                    }
+                    catch
+                    {
+                        ImagingFactory = (IWICImagingFactory)new WICImagingFactory3();
+                    }
+                }
+            return ImagingFactory;
+        }
+
+        [DllImport("shell32.dll")]
+        static extern void SHChangeNotify(HChangeNotifyEventID wEventId,
+                                      HChangeNotifyFlags uFlags,
+                                      IntPtr dwItem1,
+                                      IntPtr dwItem2);
+
+        PanasonicExif _exif;
+        WICReadOnlyStreamWrapper stream;
+
+        private PanasonicExif Exif
+        {
+            get
+            {
+                if (_exif == null)
+                    _exif = (PanasonicExif)new PanasonicRW2Decoder().DecodeExif(stream);
+                return _exif;
+            }
+        }
+
+        [ComRegisterFunction]
+        public static void OnRegistry(Type type)
+        {
+            SHChangeNotify(HChangeNotifyEventID.SHCNE_ASSOCCHANGED, HChangeNotifyFlags.SHCNF_IDLIST, IntPtr.Zero, IntPtr.Zero);
+
+            Log.Trace("GH4 RW2 WIC Decoder registered. " + type);
+        }
+
+        [ComUnregisterFunction]
+        public static void OnUnregistry(Type type)
+        {
+            Log.Trace("GH4 RW2 WIC Decoder unregistered. " + type);
+        }
+
+        public void CopyPalette(IWICPalette pIPalette)
+        {
+            Log.Trace("CopyPalette called");
+
+            throw new NotImplementedException();
+        }
+
+        public void GetColorContexts(uint cCount, ref IWICColorContext ppIColorContexts, out uint pcActualCount)
+        {
+            Log.Trace("GetColorContexts called");
+
+            throw new NotImplementedException();
+        }
+
+        public void GetContainerFormat(out Guid pguidContainerFormat)
+        {
+            Log.Trace("GetContainerFormat called");
+
+            pguidContainerFormat = new Guid("C98A5FE7-AD83-4CCA-9603-0512A8BDA6B7");
+            Log.Trace("GetContainerFormat finished");
+        }
+
+        public void GetDecoderInfo(out IWICBitmapDecoderInfo ppIDecoderInfo)
+        {
+            Log.Trace("GetDecoderInfo called");
+
+            var imagingFactory = GetImagingFactory();
+            IWICComponentInfo componentInfo;
+            var guid = GetType().GUID;
+            imagingFactory.CreateComponentInfo(ref guid, out componentInfo);
+            ppIDecoderInfo = (IWICBitmapDecoderInfo)componentInfo;
+
+            Log.Trace("GetDecoderInfo finished");
+        }
+
+        public void GetFrame(uint index, out IWICBitmapFrameDecode ppIBitmapFrame)
+        {
+            Log.Trace($"GetFrame called: {index}");
+            try
+            {
+                if (index != 0) throw new COMException("Only 0 Frame available");
+                ppIBitmapFrame = new BitmapFrameDecode(stream, Exif);
+                Log.Trace("GetFrame finished");
+            }
+            catch (Exception e)
+            {
+                Log.Error("GetFrame failed: " + e);
+                throw;
+            }
+        }
+
+        public void GetFrameCount(out uint pCount)
+        {
+            pCount = 1;
+        }
+
+        public void GetMetadataQueryReader(out IWICMetadataQueryReader ppIMetadataQueryReader)
+        {
+            Log.Trace("GetMetadataQueryReader called");
+
+            ppIMetadataQueryReader = new MetadataEnumerator(Exif);
+        }
+
+        public void GetPreview(out IWICBitmapSource ppIBitmapSource)
+        {
+            GetThumbnail(out ppIBitmapSource);
+        }
+
+        public void GetThumbnail(out IWICBitmapSource ppIThumbnail)
+        {
+            Log.Trace("GetThumbnail called");
+
+            try
+            {
+                var guid = Guid.Empty;
+
+                var pPreviewDecoder = GetImagingFactory().CreateDecoderFromStream(
+                                    new StreamComWrapper(new MemoryStream(Exif.Thumbnail)), ref guid,
+                                    WICDecodeOptions.WICDecodeMetadataCacheOnDemand);
+                IWICBitmapFrameDecode pPreviewFrame;
+                pPreviewDecoder.GetFrame(0, out pPreviewFrame);
+                ppIThumbnail = pPreviewFrame;
+                Log.Trace("GetThumbnail finished");
+            }
+            catch (Exception e)
+            {
+                Log.Error("GetThumbnail failed: " + e);
+                throw;
+            }
+        }
+
+        public void Initialize(IStream pIStream, WICDecodeOptions cacheOptions)
+        {
+            Log.Trace("Initialize called");
+
+            stream = new WICReadOnlyStreamWrapper(pIStream);
+
+            if (cacheOptions == WICDecodeOptions.WICDecodeMetadataCacheOnLoad)
+            {
+                var position = stream.Position;
+                _exif = (PanasonicExif)new PanasonicRW2Decoder().DecodeExif(new WICReadOnlyStreamWrapper(pIStream));
+                stream.Position = position;
+            }
+            Log.Trace("Initialize finished");
+        }
+
+        public void QueryCapability(IStream pIStream, out uint pdwCapability)
+        {
+            Log.Trace("QueryCapability called");
+
+            stream = new WICReadOnlyStreamWrapper(pIStream);
+            var position = stream.Position;
+            try
+            {
+                pdwCapability = (new PanasonicRW2Decoder().IsSupported(stream))
+                    ? (uint)(WICBitmapDecoderCapabilities.WICBitmapDecoderCapabilityCanDecodeThumbnail
+                    | WICBitmapDecoderCapabilities.WICBitmapDecoderCapabilityCanDecodeAllImages
+                    | WICBitmapDecoderCapabilities.WICBitmapDecoderCapabilityCanEnumerateMetadata
+                    | WICBitmapDecoderCapabilities.WICBitmapDecoderCapabilityCanDecodeThumbnail)
+                    : 0;
+            }
+            catch (Exception)
+            {
+                pdwCapability = 0;
+            }
+            finally { stream.Position = position; }
+
+        }
+
+    }
+}
